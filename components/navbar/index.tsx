6223dea6db2bd46cdf7ab1aec93500bbbac06c94
tarek-gritli <tarek.gritli@insat.ucar.tn>
2025-03-29 00:16:26 +0100

    Update home page hero section and navbar

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -6,55 +6,63 @@ import Link from "next/link";
 import { navbarLinks } from "@/constants";
 import { usePathname } from "next/navigation";
 import NavLink from "./NavLink";
+import { cn } from "@/lib/utils";
 
 export default function Navbar() {
   const pathname = usePathname();
   const isHomePage = pathname === "/";
   return (
-    <nav
-      className={`flex items-center justify-around px-6 py-4 backdrop-blur-sm border-b border-white/10 ${
-        isHomePage ? "navbar-dark" : "navbar-light"
-      }`}
-    >
-      <Link href="/" className="flex items-center gap-3">
-        {isHomePage ? (
-          <Image
-            src="/logo-horizontal-white.svg"
-            alt="MenaSYP logo"
-            width={200}
-            height={100}
-          />
-        ) : (
-          <>
-            <Image
-              src="/logo-icon-black.svg"
-              alt="MenaSYP logo"
-              width={60}
-              height={60}
-            />
-            <span className="text-primary100 font-semibold text-xl">MenaSYP</span>
-          </>
+    <div className={cn("relative", isHomePage ? "" : "bg-white")}>
+      <nav
+        className={cn(
+          "w-full z-50 transition-all duration-300 flex items-center justify-around px-6 py-4",
+          isHomePage
+            ? "absolute navbar-gradient text-white"
+            : "relative bg-white"
         )}
-      </Link>
+      >
+        <Link href="/" className="flex items-center gap-3">
+          {isHomePage ? (
+            <Image
+              src="/logo-horizontal-white.svg"
+              alt="MenaSYP logo"
+              width={200}
+              height={100}
+            />
+          ) : (
+            <>
+              <Image
+                src="/logo-icon-black.svg"
+                alt="MenaSYP logo"
+                width={60}
+                height={60}
+              />
+              <span className="text-primary100 font-semibold text-xl">
+                MenaSYP
+              </span>
+            </>
+          )}
+        </Link>
 
-      <div className="flex justify-center items-center space-x-8">
-        {navbarLinks.map((link) => (
-          <NavLink
-            key={link.route}
-            href={link.route}
-            isActive={pathname === link.route}
-            isHomePage={isHomePage}
-          >
-            {link.label}
-          </NavLink>
-        ))}
-      </div>
+        <div className="flex justify-center items-center space-x-8">
+          {navbarLinks.map((link) => (
+            <NavLink
+              key={link.route}
+              href={link.route}
+              isActive={pathname === link.route}
+              isHomePage={isHomePage}
+            >
+              {link.label}
+            </NavLink>
+          ))}
+        </div>
 
-      <div className="hidden md:flex items-center space-x-4">
-        <Button className="bg-primary100 cursor-pointer hover:bg-[#ff2040] text-white">
-          Register Now!
-        </Button>
-      </div>
-    </nav>
+        <div className="hidden md:flex items-center space-x-4">
+          <Button className="bg-primary100 cursor-pointer hover:bg-[#ff2040] text-white">
+            Register Now!
+          </Button>
+        </div>
+      </nav>
+    </div>
   );
 }
