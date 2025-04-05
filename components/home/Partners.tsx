88123b71492b04135604132faa5cec9c2d11d15a
tarek-gritli <tarek.gritli@insat.ucar.tn>
2025-04-05 21:29:51 +0100

    add scale effect on hover on partners logos

--- components/home/Partners.tsx
+++ components/home/Partners.tsx
@@ -12,7 +12,7 @@ export default function Partners() {
         <div className="flex flex-wrap justify-center md:justify-between gap-8 md:gap-4">
           {contributors.map((contributor, index) => (
             <div
-              className="flex flex-col items-center"
+              className="flex flex-col items-center group hover:scale-105 transition-transform duration-300"
               key={"contibutor-" + index}
             >
               <Image
@@ -20,6 +20,7 @@ export default function Partners() {
                 alt={contributor.altText}
                 width={200}
                 height={200}
+                className="group-hover:scale-105 transition-transform duration-300"
               />
               <h2 className="text-white font-semibold text-lg md:text-xl">
                 {contributor.title}
